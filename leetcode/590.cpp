9d973735732cb94bc7a3fb4ceba737e228ecbc79
qianlizhixiang12 <qianlizhixing_12@163.com>
2021-02-03 23:03:03 +0800

    leetcode 590

---
+++ leetcode/590.cpp
@@ -0,0 +1,33 @@
+#include <vector>
+
+using namespace std;
+
+class Node {
+public:
+  int val;
+  vector<Node *> children;
+
+  Node() {}
+
+  Node(int _val) { val = _val; }
+
+  Node(int _val, vector<Node *> _children) {
+    val = _val;
+    children = _children;
+  }
+};
+
+class Solution {
+public:
+  vector<int> result;
+  vector<int> postorder(Node *root) {
+    if (root == nullptr) {
+      return result;
+    }
+    for (Node *node : root->children) {
+      postorder(node);
+    }
+    result.push_back(root->val);
+    return result;
+  }
+};
