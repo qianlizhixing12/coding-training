46c0ebd16086c04b6d45be366c6684de438fd2e8
qianlizhixiang12 <qianlizhixing_12@163.com>
2021-03-01 23:21:36 +0800

    leetcode 78

--- leetcode/78.cpp
+++ leetcode/78.cpp
@@ -4,6 +4,7 @@ using namespace std;
 
 class Solution {
 public:
+  // 深度优先，每个元素包含的组合找齐，再找下一个
   void dfs(vector<int> &nums, vector<vector<int>> &ans, vector<bool> &help,
            vector<int> &cur, int pos) {
     ans.emplace_back(cur);
@@ -24,6 +25,25 @@ public:
     vector<int> cur;
     vector<vector<int>> ans;
     dfs(nums, ans, help, cur, 0);
+    return ans;
+  }
+};
+
+class Solution1 {
+public:
+  // 最简单方法，逐渐加元素
+  vector<vector<int>> subsets(vector<int> &nums) {
+    vector<vector<int>> ans = {vector<int>()};
+    int n = nums.size();
+    for (int i = 0; i < n; i++) {
+      int val = nums[i];
+      int len = ans.size();
+      for (int j = 0; j < len; j++) {
+        vector<int> cur = ans[j];
+        cur.emplace_back(val);
+        ans.emplace_back(cur);
+      }
+    }
     return ans;
   }
 };
