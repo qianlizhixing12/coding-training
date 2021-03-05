d176b723314068533adf584fed7d216c11459383
qianlizhixing12 <qianlizhixing_12@163.com>
2021-03-05 23:57:03 +0800

    leetcode 105

--- leetcode/105.cpp
+++ leetcode/105.cpp
@@ -37,3 +37,68 @@ public:
     return dfs(preorder, inorder, 0, inorder.size() - 1, cur, cache);
   }
 };
+
+class Solution1 {
+public:
+  // def buildTree(self, preorder: List[int], inorder: List[int]) -> TreeNode:
+  //     if len(preorder) == 0:
+  //         return None
+  //     root = TreeNode(preorder[0])
+  //     mid = inorder.index(preorder[0])
+  //     root.left = self.buildTree(preorder[1:mid + 1], inorder[:mid])
+  //     root.right = self.buildTree(preorder[mid + 1:], inorder[mid + 1:])
+  //     return root
+  /**
+   * python的代码最好理解，但是每次迭代都创建了新数组
+   * 模仿python的代码，传递左右边界，避免复制数组
+   * 传递inorder的开始位置i1用于确认mid和i1的距离
+   */
+  TreeNode *buildTree(vector<int> &preorder, vector<int> &inorder) {
+    unordered_map<int, int> cache;
+    for (int i = 0; i < inorder.size(); i++) {
+      cache[inorder[i]] = i;
+    }
+    return buildTree_(cache, preorder, inorder, 0, preorder.size() - 1, 0,
+                      inorder.size() - 1);
+  }
+
+  TreeNode *buildTree_(unordered_map<int, int> &cache, vector<int> &preorder,
+                       vector<int> &inorder, int p1, int p2, int i1, int i2) {
+    if (p1 > p2) {
+      return nullptr;
+    }
+    TreeNode *root = new TreeNode(preorder[p1]);
+    int mid = cache[preorder[p1]];
+    root->left = buildTree_(cache, preorder, inorder, p1 + 1, p1 + mid - i1, i1,
+                            mid - 1);
+    root->right = buildTree_(cache, preorder, inorder, p1 + mid - i1 + 1, p2,
+                             mid + 1, i2);
+    return root;
+  }
+};
+
+class Solution2 {
+public:
+  /**
+   * 去掉未使用的参数inorder和i2
+   */
+  TreeNode *buildTree(vector<int> &preorder, vector<int> &inorder) {
+    unordered_map<int, int> cache;
+    for (int i = 0; i < inorder.size(); i++) {
+      cache[inorder[i]] = i;
+    }
+    return buildTree_(cache, preorder, 0, preorder.size() - 1, 0);
+  }
+
+  TreeNode *buildTree_(unordered_map<int, int> &cache, vector<int> &preorder,
+                       int p1, int p2, int i1) {
+    if (p1 > p2) {
+      return nullptr;
+    }
+    TreeNode *root = new TreeNode(preorder[p1]);
+    int mid = cache[preorder[p1]];
+    root->left = buildTree_(cache, preorder, p1 + 1, p1 + mid - i1, i1);
+    root->right = buildTree_(cache, preorder, p1 + mid - i1 + 1, p2, mid + 1);
+    return root;
+  }
+};
