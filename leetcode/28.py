8c7b084106d3ae0fe6d08501efe84776cd7fd060
qianlizhixing12 <qianlizhixing_12@163.com>
2021-04-20 12:02:04 +0800

    leetcode 28

---
+++ leetcode/28.py
@@ -0,0 +1,13 @@
+class Solution:
+
+    def strStr(self, haystack: str, needle: str) -> int:
+        if not needle:
+            return 0
+        for i in range(len(haystack) - len(needle) + 1):
+            if haystack[i] == needle[0]:
+                j = 1
+                while j < len(needle) and needle[j] == haystack[i + j]:
+                    j += 1
+                if j == len(needle):
+                    return i
+        return -1
