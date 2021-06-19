836695e72316d36b14afe8fd61868ecc97690eb5
qianlizhixing12 <qianlizhixing_12@163.com>
2021-06-19 15:01:13 +0800

    leetcode Offer64

---
+++ leetcode/Offer64.py
@@ -0,0 +1,15 @@
+class Solution:
+
+    def sumNums(self, n: int) -> int:
+
+        def helper(n):
+            nonlocal result
+            n >= 1 and helper(n - 1)
+            result += n
+
+        result = 0
+        helper(n)
+        return result
+
+
+print(Solution().sumNums(3))
