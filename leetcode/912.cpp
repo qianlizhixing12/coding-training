c9b547fd436d9efef713560e280afa5450e1bc01
qianlizhixing <qianlizhixing_12@163.com>
2022-01-09 22:49:50 +0800

    leetcode 912

--- leetcode/912.cpp
+++ leetcode/912.cpp
@@ -1,3 +1,4 @@
+#include <stdlib.h>
 #include <vector>
 
 using std::vector;
@@ -34,6 +35,52 @@ public:
       }
       nums[j + 1] = tmp;
     }
+    return nums;
+  }
+
+  void mergesort(vector<int> &nums, int left, int right) {
+    if (left >= right) {
+      return;
+    }
+
+    int mid = left + ((right - left) >> 1);
+    mergesort(nums, left, mid);
+    mergesort(nums, mid + 1, right);
+    merge(nums, left, mid, right);
+  }
+
+  void merge(vector<int> &nums, int left, int mid, int right) {
+    int n = right - left + 1;
+    int *tmp = (int *)malloc(sizeof(int) * n);
+    int i = left;
+    int j = mid + 1;
+    int k = 0;
+
+    while (i <= mid && j <= right) {
+      tmp[k++] = nums[i] <= nums[j] ? nums[i++] : nums[j++];
+    }
+
+    while (i <= mid) {
+      tmp[k++] = nums[i++];
+    }
+
+    while (j <= right) {
+      tmp[k++] = nums[j++];
+    }
+
+    for (k = 0; k < n; ++k) {
+      nums[left + k] = tmp[k];
+    }
+
+    free(tmp);
+  }
+
+  vector<int> sortArray_mergesort(vector<int> &nums) {
+    int l = 0;
+    int r = nums.size() - 1;
+
+    mergesort(nums, l, r);
+
     return nums;
   }
 };
