48ad6d8b8da3d9f4781003953653b79ff9ac887a
qianlizhixing12 <qianlizhixing_12@163.com>
2022-02-21 23:11:57 +0800

    leetcode 1442

--- leetcode/1442.py
+++ leetcode/1442.py
@@ -1,32 +1,16 @@
-from typing import List
-
-
 class Solution:
+
     def countTriplets(self, arr: List[int]) -> int:
-        ans = 0
-        n = len(arr)
+        cache = [0]
+        result = 0
 
-        dp = [0] * (n + 1)
+        for num in arr:
+            cache.append(cache[-1] ^ num)
+
+        n = len(cache)
         for i in range(n):
-            dp[i + 1] = dp[i] ^ arr[i]
-
-        for i in range(n - 1):
             for j in range(i + 1, n):
-                for k in range(j, n):
-                    if dp[i] == dp[k + 1]:
-                        ans += 1
-        return ans
+                if cache[i] == cache[j]:
+                    result += j - i - 1
 
-    def countTriplets_(self, arr: List[int]) -> int:
-        ans = 0
-        n = len(arr)
-
-        dp = [0] * (n + 1)
-        for i in range(n):
-            dp[i + 1] = dp[i] ^ arr[i]
-
-        for i in range(n - 1):
-            for k in range(i + 1, n):
-                if dp[i] == dp[k + 1]:
-                    ans += k - i
-        return ans
+        return result
