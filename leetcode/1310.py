75d9fe475925815fc4fd23e8b002c884a13a52ef
qianlizhixing12 <qianlizhixing_12@163.com>
2022-02-21 22:40:08 +0800

    leetcode 1310

--- leetcode/1310.py
+++ leetcode/1310.py
@@ -4,12 +4,17 @@ from typing import List
 class Solution:
 
     def xorQueries(self, arr: List[int], queries: List[List[int]]) -> List[int]:
-        dp = [0]
+        cache = [0]
+
         for num in arr:
-            dp.append(dp[-1] ^ num)
+            cache.append(cache[-1] ^ num)
 
         ans = []
+
         for query in queries:
-            ans.append(dp[query[1] + 1] ^ dp[query[0]])
+            ans.append(cache[query[0]] ^ cache[query[1] + 1])
 
         return ans
+
+
+Solution().xorQueries([1, 3, 4, 8], [[0, 1], [1, 2], [0, 3], [3, 3]])
